7a1763096bb0303dba73ad411de62517cf78e79d
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-01-19 14:50:48 +0700

    feat: add project github user search This project was created using Github API and that will fetch the data about the user and repos

---
+++ project-10/github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "367bec0073f14028ff19";
+    this.client_secret = "9ce7c3c84137154a3efec8a2e0d63a0f38baa0e4";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
